db19abfc4d0a87aabc82987344a1929ad92fed8d
nicatelyv <nikoalyv@gmail.com>
2024-05-13 11:16:12 +0400

    Auth Navbar, language function added

--- euphoria-front/src/pages/Auth/Auth.jsx
+++ euphoria-front/src/pages/Auth/Auth.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import AuthNavbar from '../../layout/navbar/AuthNavbar'
 
 export const Auth = () => {
   return (
     <div>
-      <h1>Auth</h1> {/* Burada auth navbar olacaq */}
-      <Outlet/>
+      <AuthNavbar />
+      <Outlet />
     </div>
   )
 }
