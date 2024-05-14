4d1811a182f0c434dc023480edbfede2a6ddc663
Afsana <abdullayevaynur527@gmail.com>
2024-05-14 23:34:12 +0400

    checkEmail and verification end

--- euphoria-front/src/router/router.jsx
+++ euphoria-front/src/router/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home, Auth } from "../pages"
 import Signin from "../components/sign in/Signin";
+import CheckEmail from "../components/check email/CheckEmail";
+import Verification from "../components/verification/Verification";
 
 export const Router = createBrowserRouter([
     {
@@ -23,9 +25,11 @@ export const Router = createBrowserRouter([
             },
             {
                 path: "/auth/check-email",
+                element:<CheckEmail/>
             },
             {
                 path: "/auth/verification",
+                element:<Verification/>
             },
             {
                 path: "/auth/create-new-password",
