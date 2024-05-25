bf5ca8a423d5cb9b70563bbe728172972cebd443
Afsana <abdullayevaynur527@gmail.com>
2024-05-25 17:45:48 +0400

    create fashion section and new arrival section

--- euphoria-front/src/pages/Home/Home.jsx
+++ euphoria-front/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import HomeSlide from "../../components/Home/Slide/HomeSlide";
 import SaleProducts from "../../components/Home/SaleProducts/SaleProducts";
 import NewArrival from "../../components/Home/NewArrival/NewArrival";
 import Footer from "../../layout/footer/Footer";
+import FashionSection from "../../components/Home/FashionSection/FashionSection";
 import HomeNavbar from "../../layout/navbar/HomeNavbar";
 import BigZone from "../../components/Home/BigZone/BigZone";
 
@@ -13,6 +14,7 @@ export const Home = () => {
       <HomeSlide />
       <SaleProducts />
       <NewArrival />
+      <FashionSection/>
       <BigZone />
       <Footer />
     </>
