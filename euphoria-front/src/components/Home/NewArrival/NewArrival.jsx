3ebf8b96955f3ae1fc3dd9f437552686fd77a2f8
nicatelyv <nikoalyv@gmail.com>
2024-05-26 01:52:02 +0400

    bug fix

--- euphoria-front/src/components/Home/NewArrival/NewArrival.jsx
+++ euphoria-front/src/components/Home/NewArrival/NewArrival.jsx
@@ -1,22 +1,57 @@
-import React from 'react'
-import newArrivalStyle from './NewArrival.module.scss'
+import React from "react";
+import newArrivalStyle from "./NewArrival.module.scss";
 import Slider from "react-slick";
+import { useTranslation } from "react-i18next";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
-function SampleArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "gray", borderRadius: "100%" }}
-      onClick={onClick}
-    />
-  );
-}
-
 function NewArrival() {
+  const { t } = useTranslation();
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        onClick={onClick}
+        style={{
+          ...style,
+          width: "20px",
+          height: "20px",
+          fontSize: "20px",
+          textAlign: "center",
+          position: "absolute",
+          top: "50%",
+          transform: "translate(0, -50%)",
+        }}
+      >
+        <i style={{ color: "black" }} class="fa-solid fa-angle-right"></i>
+      </div>
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        onClick={onClick}
+        style={{
+          ...style,
+          width: "20px",
+          height: "20px",
+          fontSize: "20px",
+          textAlign: "center",
+          position: "absolute",
+          top: "50%",
+          transform: "translate(0, -50%)",
+        }}
+      >
+        <i style={{ color: "black" }} class="fa-solid fa-angle-left"></i>
+      </div>
+    );
+  }
+
   var settings = {
     dots: false,
     infinite: true,
@@ -24,8 +59,8 @@ function NewArrival() {
     slidesToShow: 4,
     slidesToScroll: 4,
     initialSlide: 0,
-    nextArrow: <SampleArrow />,
-    prevArrow: <SampleArrow />,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
     responsive: [
       {
         breakpoint: 1024,
@@ -33,104 +68,103 @@ function NewArrival() {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
+
   return (
     <div className={newArrivalStyle.container}>
       <div className={newArrivalStyle.context}>
         <div className={newArrivalStyle.line}></div>
-        <h2>New Arrival</h2>
+        <p>New Arrival</p>
       </div>
       <div className="slider-container">
-        <Slider {...settings} className='sldr'>
+        <Slider {...settings} className="sldr">
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img1.png" alt="slide imgages for react..." />
-            <p>Knitted Joggers</p>
+            <p>{t("Knitted Joggers")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img2.png" alt="slide imgages for react..." />
-            <p>Full Sleeve</p>
+            <p>{t("Full Sleeve")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img3.png" alt="slide imgages for react..." />
-            <p>Active T-Shirts</p>
+            <p>{t("Active T-Shirts")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img4.png" alt="slide imgages for react..." />
-            <p>Urban Shirts</p>
+            <p>{t("Urban Shirts")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img1.png" alt="slide imgages for react..." />
-            <p>Knitted Joggers</p>
+            <p>{t("Knitted Joggers")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img2.png" alt="slide imgages for react..." />
-            <p>Full Sleeve</p>
+            <p>{t("Full Sleeve")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img3.png" alt="slide imgages for react..." />
-            <p>Active T-Shirts</p>
+            <p>{t("Active T-Shirts")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img4.png" alt="slide imgages for react..." />
-            <p>Urban Shirts</p>
+            <p>{t("Urban Shirts")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img1.png" alt="slide imgages for react..." />
-            <p>Knitted Joggers</p>
+            <p>{t("Knitted Joggers")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img2.png" alt="slide imgages for react..." />
-            <p>Full Sleeve</p>
+            <p>{t("Full Sleeve")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img3.png" alt="slide imgages for react..." />
-            <p>Active T-Shirts</p>
+            <p>{t("Active T-Shirts")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img4.png" alt="slide imgages for react..." />
-            <p>Urban Shirts</p>
+            <p>{t("Urban Shirts")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img1.png" alt="slide imgages for react..." />
-            <p>Knitted Joggers</p>
+            <p>{t("Knitted Joggers")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img2.png" alt="slide imgages for react..." />
-            <p>Full Sleeve</p>
+            <p>{t("Full Sleeve")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img3.png" alt="slide imgages for react..." />
-            <p>Active T-Shirts</p>
+            <p>{t("Active T-Shirts")}</p>
           </div>
           <div className={newArrivalStyle.sliderContainer}>
             <img src="/img/Img4.png" alt="slide imgages for react..." />
-            <p>Urban Shirts</p>
+            <p>{t("Urban Shirts")}</p>
           </div>
         </Slider>
       </div>
     </div>
-  )
+  );
 }
 
-export default NewArrival
-
-
+export default NewArrival;
