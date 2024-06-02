2de37e9afac113c9da41aeba37b67dcb77f1890e
Afsana <abdullayevaynur527@gmail.com>
2024-06-02 09:09:20 +0400

    list best price

--- euphoria-front/src/components/ProductList/ListBestPrice/ListBestPrice.jsx
+++ euphoria-front/src/components/ProductList/ListBestPrice/ListBestPrice.jsx
@@ -14,7 +14,66 @@ function ListBestPrice() {
             {t("Buy Women's Clothing at Best Price")}
           </h2>
       </div>
-      <div className={listBestPriceStyle.bodyCol}></div>
+      <div className={listBestPriceStyle.bodyCol}>
+        <table border={0}>
+        <thead>
+          <tr>
+            <th>Women's Clothing</th>
+            <th>Best Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Pick Any 4- Womens Plain T-shirt Combo</td>
+            <td>₹1099</td>
+          </tr>
+          <tr>
+            <td>Pick Any 4- Plain Womens Boxer Combo</td>
+            <td>₹1099</td>
+          </tr>
+          <tr>
+            <td>Pick Any 4 - Women Plain Full Sleeve T-shirt Combo</td>
+            <td>₹1399</td>
+          </tr>
+          <tr>
+            <td>Multicolor Checkered Long Casual Shirts for Women</td>
+            <td>₹499</td>
+          </tr>
+          <tr>
+            <td>Pick Any 2: Plain Boxy Casual Shirts for Women Combo</td>
+            <td>₹799</td>
+          </tr>
+          <tr>
+            <td>Blue Floral Anarkali Kurti</td>
+            <td>₹599</td>
+          </tr>
+          <tr>
+            <td>Jade Black Narrow Cut Flexible Women Jeggings</td>
+            <td>₹998</td>
+          </tr>
+          <tr>
+            <td>Mustard-yellow Solid Straight-Fit Women Pant</td>
+            <td>₹449</td>
+          </tr>
+          <tr>
+            <td>Women Pants Combo - Pick Any 2</td>
+            <td>₹800</td>
+          </tr>
+          <tr>
+            <td>Pista Green Solid Boxy Casual Shirts for Women</td>
+            <td>₹449</td>
+          </tr>
+          <tr>
+            <td>Plain Burgundy Womens Boxer</td>
+            <td>₹349</td>
+          </tr>
+          <tr>
+            <td>Striped Front Tie Casual Shirts for Women</td>
+            <td>₹449</td>
+          </tr>
+        </tbody>
+        </table>
+      </div>
       </div>
     </div>
   )
