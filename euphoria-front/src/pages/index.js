4f9aa5ae4fc1b1383f2fe292738c4f1337a76832
rustamova148 <79011363+rustamova148@users.noreply.github.com>
2024-06-09 00:49:07 +0400

    detail properties added

--- euphoria-front/src/pages/index.js
+++ euphoria-front/src/pages/index.js
@@ -1,9 +1,5 @@
 export * from "./Home";
-<<<<<<< HEAD
 export * from "./Auth";
 export * from "./Women";
 export * from "./Detail";
-=======
-export * from "./Auth"
 export * from "./Checkout"
->>>>>>> ce09cda81b2b96835f26dc369c6aaadc56150c5a
