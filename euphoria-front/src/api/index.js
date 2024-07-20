ad336bc444da76fd80ececd0fcc0d4e954315bf7
hazratahmdzd <B062192021@asoiu.edu.az>
2024-07-20 20:46:47 +0400

    updated product-post api

--- euphoria-front/src/api/index.js
+++ euphoria-front/src/api/index.js
@@ -1,17 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = 'euphoria-ecommerce.onrender.com/api'
+const baseURL = "euphoria-ecommerce.onrender.com/api";
 
-export const axiosFunction = async ( method, endpoint, data ) => {
-
-    try {
-        const response = await axios({
-            method,
-            url: `https://${baseURL}${endpoint}`,
-            data: data && data
-        });
-        return response.data;
-    } catch(err) {
-        console.log(err.message)
-    }
-}
+export const axiosFunction = async (method, endpoint, data) => {
+  try {
+    const response = await axios({
+      method,
+      url: `https://${baseURL}${endpoint}`,
+      data: data && data,
+    });
+    return response.data;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
